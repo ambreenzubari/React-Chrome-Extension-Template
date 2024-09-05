ab6b66bf2d79936ef66c2a87ea93c29b9220b2ff
ambreen zubari <ambreenzubari3ss@gmail.com>
2024-09-05 16:12:52 +0500

    added content script

---
+++ dist/contentScript.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+/*!********************************************!*\
+  !*** ./src/contentScript/contentScript.ts ***!
+  \********************************************/
+window.onload=()=>{
+    console.log("I am from Coontent SCript")
+}
+/******/ })()
+;
+//# sourceMappingURL=contentScript.js.map
